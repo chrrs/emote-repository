11a02e391f905f3079cc2c94d9907797b158612b
chrrs <git@chrr.me>
2024-06-09 14:59:05 +0200

    fix: purge cache when deleting an emote

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,6 @@
 import * as db from '$lib/db.server';
 import { fail, redirect } from '@sveltejs/kit';
+import { purgeCache } from '@netlify/functions';
 
 export async function load() {
 	return {
@@ -22,6 +23,12 @@ export const actions = {
 		const emoteId = body['id'];
 
 		const emote = await db.emotes.delete(user, emoteId);
+
+		try {
+			await purgeCache();
+		} catch (e) {
+			console.warn('could not purge netlify cache:', e);
+		}
 
 		if (emote) {
 			console.log(`${user.name} deleted the emote ${emote.name}`);
